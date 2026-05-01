0ea0d8589e65c9dfdcd486126e62d1b8ab62f7e6
cheikhibra26 <wwwcheikhibrandiaye@gmail.com>
2026-05-01 20:17:12 +0200

    Update cv.py

--- cv.py
+++ cv.py
@@ -3,28 +3,30 @@ import streamlit as cv
 cv .set_page_config(page_title="cv - CHEIKH IBRA NDIAYE", page_icon="photo.pjg",layout= "centered")#
 # titre
 cv.title("CHEIKH IBRA NDIAYE")
-cv. write ("Ingénieur en  Aménagemnagement et gestionnaire urbaine en dote dune compétence de planification et aménagement urbaine du territoire  .Expert, Téchnicien superieur  en géomatique avec la maitrise des outils dinformation géographie (SIG),cartographie,analyse des données spatial ,dessin architecture ( plan dhabitation ,commercial) et programmation, base donnée.Télédection comtrole drone,Topographe. Je suis certifié sur en collect de données quantitative au niveau de lANSD en plus jai aqui les compétences en labaratoir géotechnique  ")
+cv. write ("Ingénieur en  Aménagemnagement et gestionnaire urbaine en Afrique doté d'une compétence de planification et aménagiste urbain du territoire  .Téchnicien superieur  en géomatique avec la maitrise des outils dinformation géographie (SIG),cartographie,analyse des données spatial ,dessin architecture ( plan dhabitation ,commercial) et programmation, creation d'une base de donnée,télédection,Pilote drone,Topographe. Je suis certifié sur la  collect de données quantitative et qualitative au niveau de l'ANSD. En plus, j'ai aqui des compétences en labaratoir géotechnique  ")
 
 cv.title("Parcours")
 
-cv.write("Master 2 en Aménagement et Gestion Urbaine en Afrique à ľuniversité Cheikh Anta Diop de Dakar")
+cv.write("Master 2 en Aménagement et Gestion Urbaine en Afrique")
 cv. write ("Université  Cheikh Anta Diop de Dakar ")
-cv.write ("Année:2025/2026")
-cv.write(" Premier Année-BTS Geomatique  ")
-cv. write ("Centrte Dentrepreneuriat et de Developpement Technique ")
-cv.write ("Année 2025/2026")
-cv.write("Licence en Gouvernance Territoriale et Développement Local Option en Aménagement du Territoire et Développement Local ")
-cv. write ("Universite  Cheikh Anta Diop de Dakar (UCAD) / Institut de la Gouvernance Territorial et Développement Local (IGT)")
-cv.write ("Année 2025/2026")
+cv.write ("Année:2026")
+cv.write("Licence en Gouvernance Territoriale et Développement Local Option:
+         ("Aménagement du Territoire et Développement Local ")
+cv. write ("Université  Cheikh Anta Diop de Dakar (UCAD):
+                         ("Institut de la Gouvernance Territorial et du  Développement Local (IGT)")
+cv.write ("Année 2024")
+cv.write("Brévet Téchnique Supérieure en  Géomatique  ")
+cv. write ("Centrte D'entrepreneuriat et de Développement Téchnique"("CEDT":"Le G15") ")
+cv.write ("Année 2027)
 cv.write("Baccalauriat en Science Humaine et Social ")
-cv. write ("Lycee Mame Thierno Birahim Mbacke")
+cv. write ("Université Cheikh Anta Diop de Dakar (UCAD)")
 cv.write ("Année:2020")
 
 cv.title ("Certification")
 
-cv.write(" Attestation en Laboratoir Geotechnique ")
-cv. write ("CFPT-Castor")
-cv.write("Attestation en Technologie Immersive VR/AR/MR ")
+cv.write(" Attestation en Laboratoir Géotechnique ")
+cv. write ("Centre de Formation Professionnel des Travaux Public ("CFPT")
+cv.write("Attestation en Téchnologie Immersive VR/AR/MR ")
 cv. write ("CURI")
 
 cv. write ("Attestation en Infographie")
@@ -33,36 +35,34 @@ cv. write (" Attestation en Prestation de Service Rnu")
 cv.write ("ANSD ")
 cv. write (" Attestation en Informatique et Internet ")
 cv.write ("FORCE-N")
-cv. write ("Attestation en Intelligence le Pour Tous")
+cv. write ("Attestation en Intélligence Artificielle  Pour Tous")
 cv.write (" FORCE-N ")
-cv. write (" Attestation en Traitement de Donnee")
-cv.write (" FORCE-N ")
-cv.write ("Année 2024")
-cv.write (" Attestation du Projet de Formation sur la Valorisation des Ressource et Innovation Expoitation Economique du Senegal PFVRIEES") 
+cv. write (" Attestation en Traitement de Donnée")
+cv.write (" FORCE-N")
+cv.write (" Attestation du Projet de Formation sur la Valorisation des Ressources et Innovation Exploitation Economique du Sénégal ("PFVRIEES") 
 cv.write ("UNCHK")
 
 cv.title ("COMPETENCES")
 cv.markdown("""
-- Microsofte office
-- Cartograp Topographie 
-- Planification
-- Déssin Plan:
-    *Auto-cad
-- Collecte données
-- Base de données spatiales
+- Microsofte office:("Word,Exel,Power Point")
+- Cartographie 
+-Topographie 
+- Planification Territoriale 
+- Dessin Plan Architecture
+- Collécte données:("Quantitative,Qualitative")
+- Base de données:
 - Aménagement du Territoire
-- Planification du Territoire
-- Diagnostique Spatiale
-- Essaie Laboratoire Gétechnique
+- Diagnostique Spatiale,Projection spatiale
+- Essaie en  Laboratoire Géotechnique
 - Infographie:("
     Carte visite,invitation,flyers
     ")
-- Numerisation 
+- Numérisation 
 - Programmation("Anacondant Python Jupterlab streamlit
 """)
 with cv.sidebar:
     cv.title("CHEIKH IBRA NDIAYE")             
-    cv.write("Ingénieur Aménagiste/Technicien en Géomatique")  
+    cv.write("Ingénieur Aménagiste/Téchnicien en Géomatique")  
     cv.write("Email:wwwcheikhibrandiaye@gmail.com")
     cv.write("Adresse:Dakar/Sénégal")
     cv.write("Situation Matrimonial:Célibataire sans enfant")
