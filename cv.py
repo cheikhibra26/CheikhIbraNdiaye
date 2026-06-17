dedbe9f8a94a31ba6b41f818544d629df148608d
cheikhibra26 <wwwcheikhibrandiaye@gmail.com>
2026-06-17 20:29:36 +0200

    Update cv.py

--- cv.py
+++ cv.py
@@ -8,7 +8,7 @@ cv. write ("Ingénieur en  aménagegement et gestion urbaine en Afrique doté d'
 
 cv.title("PARCOURS")
 
-cv.subhider ("Master en Aménagement et Gestion Urbaine en Afrique")
+cv.write ("Master en Aménagement et Gestion Urbaine en Afrique")
 cv. write ("Université  Cheikh Anta Diop de Dakar ")
 cv.write ("Année:2026")
 cv.title("Licence en Gouvernance Térritoriale et Développement Local Option:Aménagement du Territoire et Développement Local ")
